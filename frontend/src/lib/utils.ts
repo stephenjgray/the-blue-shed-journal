6040b0a7583ca2d724621e158aa3039b6d0ba8c2
stephen gray <stephenjgray79@gmail.com>
2025-07-09 20:32:00 +0100

    quick fix on utility method.

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,5 +1,6 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { i18n } from "@/i18n/i18n-config"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
